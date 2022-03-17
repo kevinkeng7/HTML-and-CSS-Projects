e8ab03af0a21f8a132d621722528df04664c22f5
kevinkeng7 <68366086+kevinkeng7@users.noreply.github.com>
2022-03-17 00:39:23 -0700

    Submission Assignment #2
    Minimum project requirements:
    
    Utilize alt attribute
    Utilize <img> element
    Utilize <b> tag
    Utilize <i> tag
    Utilize <u> tag

---
+++ Basic_HTML_and_JavaScript_1/Basic_JavaScript_1.js
@@ -0,0 +1,7 @@
+alert("Hello, World! I am an alert box!");
+
+
+				var String = "Kiss me, I'm Irish!";
+				var result = String.fontcolor("green");
+				document.getElementById("Irish").innerHTML = 
+				result;
